8c87a4b4737958478f461de7e9b7b33cad9f642a
Ozan <ozancck@icloud.com>
2022-08-21 22:56:02 +0300

    Sort By Category

--- src/App.js
+++ src/App.js
@@ -13,11 +13,17 @@ export default class App extends Component {
     }
 
     changeCategory=category=>{
-        this.setState({currentCategory: category.categoryName})
+        this.setState({currentCategory: category.categoryName});
+        this.getProducts(category.id)
     }
 
-    getProducts =()=>{
-        fetch("http://localhost:3000/products")
+    getProducts =(categoryId)=>{
+        let url = "http://localhost:3000/products"
+        if (categoryId){
+            url+="?categoryId="+categoryId
+        }
+
+        fetch(url)
             .then(response=>response.json())
             .then(data=>this.setState({products: data}))
     }
