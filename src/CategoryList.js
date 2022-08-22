162d6f65fd981c935105b025bb6871e160659ab6
Ozan <ozancck@icloud.com>
2022-08-22 22:13:31 +0300

    active

--- src/CategoryList.js
+++ src/CategoryList.js
@@ -24,6 +24,7 @@ export default class CategoryList extends Component {
                 <ListGroup>
                     {this.state.categories.map(category =>
                         <ListGroupItem
+                            active={category.categoryName === this.props.currentCategory}
                             onClick={() => this.props.changeCategory(category)}
                             key={category.id}>
                             {category.categoryName}
