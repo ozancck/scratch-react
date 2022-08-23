d666919d34cb70692efea3591f128f6c6d53e350
Ozan <ozancck@icloud.com>
2022-08-23 22:09:34 +0300

    Empty Cart

--- src/CartSummary.js
+++ src/CartSummary.js
@@ -1,29 +1,41 @@
 import React, {Component} from 'react';
-import {Badge, DropdownItem, DropdownMenu, DropdownToggle, UncontrolledDropdown} from "reactstrap";
+import {Badge, DropdownItem, DropdownMenu, DropdownToggle, UncontrolledDropdown, NavItem, NavLink} from "reactstrap";
 
 class CartSummary extends Component {
+    renderSummary() {
+        return (<UncontrolledDropdown nav inNavbar>
+            <DropdownToggle nav caret>
+                Your Cart - {this.props.cart.length}
+            </DropdownToggle>
+            <DropdownMenu right>
+                {
+                    this.props.cart.map(cartItem => (
+                        <DropdownItem key={cartItem.product.id}>
+                            {cartItem.product.productName}
+                            <Badge color="success">{cartItem.quantity}> </Badge>
+                        </DropdownItem>
+                    ))
+                }
+                <DropdownItem divider/>
+                <DropdownItem>
+                    Reset
+                </DropdownItem>
+            </DropdownMenu>
+        </UncontrolledDropdown>)
+    }
+
+    renderEmptyCart() {
+        return(
+            <NavItem>
+                <NavLink>Empty Cart </NavLink>
+            </NavItem>
+        )
+    }
+
     render() {
         return (
             <div>
-                <UncontrolledDropdown nav inNavbar>
-                    <DropdownToggle nav caret>
-                        Your Cart - {this.props.cart.length}
-                    </DropdownToggle>
-                    <DropdownMenu right>
-                        {
-                            this.props.cart.map(cartItem => (
-                                <DropdownItem key={cartItem.product.id}>
-                                    {cartItem.product.productName}
-                                    <Badge color="success">{cartItem.quantity}> </Badge>
-                                </DropdownItem>
-                            ))
-                        }
-                        <DropdownItem divider/>
-                        <DropdownItem>
-                            Reset
-                        </DropdownItem>
-                    </DropdownMenu>
-                </UncontrolledDropdown>
+                {this.props.cart.length > 0 ? this.renderSummary() : this.renderEmptyCart()}
             </div>
         );
     }
